c768f590c005011d21add837ef21b738c29045c8
Diego Mendes <mendesdiego818@gmail.com>
2026-06-06 23:52:20 -0300

    Home section finished -mobile included

--- We-26/src/App.jsx
+++ We-26/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.scss";
 import { Header } from "./components/Header/Header";
 import { Background } from "./components/Background/Background";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Background></Background>
+      <Home></Home>
     </>
   );
 }
---
+++ We-26/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './home.scss';
+
+export function Home(){
+    return(
+        <section className="home">
+            <p className='home__subtitle'>Official Sticker Collection</p>
+            <h1 className="home__title">We Are <br/> #26 </h1>
+            <p className="home__description">Toque nas figurinhas que você já colou e acompanhe sua coleção da Copa do Mundo de 2026 em tempo real. Entre na sua conta para salvar e sincronizar entre dispositivos.</p>
+        </section>
+    )
+}
